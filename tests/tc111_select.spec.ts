b5b46eff6199cd8c730942f29cf67a42cd574305
660710595 <139957506+660710595@users.noreply.github.com>
2026-04-20 18:19:14 +0700

    Update tc111_select.spec.ts

--- tests/tc111_select.spec.ts
+++ tests/tc111_select.spec.ts
@@ -2,6 +2,16 @@
 import { test, expect } from '@playwright/test';
 
 test('TC111: search department should show result', async ({ page }) => {
+    await page.goto('https://project-superend-cen8.vercel.app/',);
+    const USERNAME = 'Admin1';
+    const PASSWORD = 'examroom@1234';
+
+    await page.getByRole('textbox', { name: 'ใส่ชื่อบัญชีผู้ใช้' }).fill(USERNAME);
+    await page.getByRole('textbox', { name: 'ใส่รหัสผ่านของคุณ' }).fill(PASSWORD);
+    await page.getByRole('button', { name: 'ล็อกอิน' }).click();
+    await page.waitForNavigation();
+
+    await expect(page).toHaveURL(/.*Home/);
     await page.getByRole('button', { name: 'เปิดเมนู' }).click();
     await page.getByRole('link', { name: 'ให้สิทธิ์กรรมการห้องอำนวยการสอบ' }).click();
 
